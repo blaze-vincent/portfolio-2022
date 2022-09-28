2a7225620d8908e787fa14d638b9b484e52f6fab
blaze@drakehs <blaze.vincent@drakehs.com>
2022-09-28 13:48:57 -0500

    Add _id field for querying

--- db/models/session.js
+++ db/models/session.js
@@ -1,8 +1,9 @@
 import { Schema, model, models } from 'mongoose';
 
 const SessionSchema = new Schema({
+  _id: String,
   expires: Date,
-  session: String
+  session: String,
 })
 
 module.exports = models.Session || model('Session', SessionSchema)
