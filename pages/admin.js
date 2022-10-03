15cf2192217064bf039bcd8cc704e56395bbfeb0
blaze@drakehs <blaze.vincent@drakehs.com>
2022-10-03 15:01:29 -0500

    Abstract ssp authentication method

--- pages/admin.js
+++ pages/admin.js
@@ -1,44 +1,7 @@
-import cookieIsValid from "../utils/cookieIsValid";
-import sessionIsAuthenticated from "../utils/sessionIsAuthenticated";
+import getServerSidePropsAuthentication from "../middleware/getServerSidePropsAuthentication";
 
 export async function getServerSideProps(ctx){
-  const unauthorized = {
-    redirect: {
-      destination: '/',
-      permanent: false
-    }
-  }
-
-  const cookie = ctx.req.cookies['connect.sid'];
-
-  if(!cookie){
-    return unauthorized
-  }
-  
-  const cookieRegex = /s%.*\./i;
-  if(!cookieRegex.test(cookie)){
-    return unauthorized
-  }
-
-  const cookieComponents = cookie.split(':')[1].split('.');
-  const sid = cookieComponents[0];
-  const signature = cookieComponents[1];
-
-  if(!cookieIsValid(sid, signature)){
-    //cookie signature is invalid - hijacking likely
-    return unauthorized
-  }
-
-  if(sessionIsAuthenticated(sid)){
-    return {
-      props: {
-        authenticated: true
-      }
-    }
-  }
-
-  return unauthorized
-
+  return getServerSidePropsAuthentication(ctx.req.cookies['connect.sid']);
 }
 
 export default function Admin({authenticated}){
