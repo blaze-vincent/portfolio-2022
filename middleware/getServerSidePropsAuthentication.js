ed02b1a429ebb85b33c18825b410fc1440a7df7a
blaze@drakehs <blaze.vincent@drakehs.com>
2022-10-03 15:01:52 -0500

    Abstract ssp auth and clean up logic

--- middleware/getServerSidePropsAuthentication.js
+++ middleware/getServerSidePropsAuthentication.js
@@ -1,3 +1,29 @@
-export default function sessionIsAuthorized(){
+import cookieIsValid from "../utils/cookieIsValid";
+import sessionIsAuthenticated from "../utils/sessionIsAuthenticated";
+
+export default function getServerSidePropsAuthentication(cookie){
   //abstract methods performed in getserverside props on /admin
+  const cookieRegex = /s:.*\..*/i;
+  if(cookieRegex.test(cookie)){
+    const cookieComponents = cookie.split(':')[1].split('.');
+    const sid = cookieComponents[0];
+    const signature = cookieComponents[1];
+
+    if(cookieIsValid(sid, signature)){
+      if(sessionIsAuthenticated(sid)){
+        return {
+          props: {
+            authenticated: true
+          }
+        }
+      }
+    }
+  }
+
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false
+    }
+  }
 }
