bc8bdd0ec8cc537793e0db71e7e098bf3f7d6443
blaze@drakehs <blaze.vincent@drakehs.com>
2022-10-10 15:48:35 -0500

    Increase header font

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 export default function Header(){
   return (<header>
-    <h1 className="text-blue-700 text-2xl font-bold">
+    <h1 className="text-4xl font-bold">
       header tag content
     </h1>
   </header>)
